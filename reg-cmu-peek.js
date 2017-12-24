047d2118a69801479de0cc8238109e23bce08612
tmwatchanan <tmwatchanan@gmail.com>
2017-12-25 00:15:00 +0700

    Add showing each student's picture BUT still has some errors

--- reg-cmu-peek.js
+++ reg-cmu-peek.js
@@ -13,8 +13,44 @@ if (document.title.indexOf("Students enrollment in course") != -1) {
     // $('.msan').html('Whatever <b>HTML</b> you want here.');
     $('.msan').eq(3).html('RegCmu Peek');
     var studentIdList = [];
-    $('html body center div table tbody tr').each(function() { studentIdList.push($(this).text()) });
-    console.log(studentIdList);
-    // $( "tbody tr.msan td:nth-child(0)" ).append( "<span> - pic here!</span>" );
-    // $( "tr td:nth-child(4)" ).append( "<span> - pic here!</span>" );
+    // $('html body center div table tbody tr').each(function() { studentIdList.push($(this).text()) });
+    var tableObject = $("html body center div table tbody tr.msan")
+    .clone()
+    .find('td:first').remove().end()
+    .find('td:last').remove().end()
+    .children()
+    .clone()
+    .children()
+    .remove()	//remove all the children
+    .end()	//again go back to selected element
+    tableObject.each(function() { studentIdList.push($(this).text()) });
+    filtered = studentIdList.filter(function(el, index) {
+        return index % 2 === 0;
+    });
+    // console.log(studentIdList);
+    console.log(filtered);
+
+    var studentDataList = [];
+    var studentPictureList = [];
+    for (var i = 0; i < filtered.length; i++) {
+        var year = filtered[i].substr(0, 2);
+        var faculty = filtered[i].substr(2, 2);
+        var program = filtered[i].substr(4, 1);
+        var id = filtered[i];
+        // console.log([year, faculty, program, id]);
+        var picUrl = 'http://archive3.lib.cmu.ac.th/innopac/patrons/';
+        picUrl += year + '/';
+        if (year != '58') {
+            picUrl += program + '/';
+        }
+        picUrl += faculty + '/' + id + '.jpg';
+        studentDataList.push([year, faculty, program, id]);
+        studentPictureList.push(picUrl);
+
+        $( "tr.msan:contains(" + id + ") td:nth-child(4)" ).html("<img src=\"" + studentPictureList[i] + "\">");
+    }
+    console.log(studentPictureList);
+
+    // $( "tr.msan td:nth-child(4)" ).append( "<img src=\"" + studentPictureList[0] + "\">" );
+    // $( "tr.msan td:nth-child(4)" ).append( "<span>pic here!</span>" );
 }
